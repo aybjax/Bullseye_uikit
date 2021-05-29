e6e762949050df99dde131b1bbf076ffa43d9d06
aybjax <aybat93@gmail.com>
2021-05-30 00:47:38 +0600

    Connected outlets

--- Bullseye_uikit/ViewController.swift
+++ Bullseye_uikit/ViewController.swift
@@ -11,19 +11,52 @@ class ViewController: UIViewController {
     // Properties
     // ==========
     
-    var currentValue: Int = 1
+    var currentValue: Int = 0
+    var targetValue: Int = 0
+    var score: Int = 0
+    var round = 0
+    
+    // Outlets
+    // =======
+    
+    @IBOutlet weak var slider: UISlider!
+    @IBOutlet weak var targetLabel: UILabel!
+    @IBOutlet weak var scoreLabel: UILabel!
+    @IBOutlet weak var roundLabel: UILabel!
     
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        startNewRound()
+//        updateTotalScoreLable()
     }
     
     // Methods
     // =======
+    
+    func startNewRound() {
+        targetValue = Int.random(in: 1...100)
+        currentValue = 50
+        slider.value = Float(currentValue)
+        round += 1
+        updateLabel()
+    }
+    
+    func updateLabel() {
+        targetLabel.text = String(targetValue)
+        scoreLabel.text = String(score)
+        roundLabel.text = String(round)
+    }
+    
+    // Actions
 
     @IBAction func showAlert() {
-        let msg = "The value of the slider is: \(currentValue)"
+        let difference = abs(targetValue - currentValue)
+        let points = 100 - difference
+        score += points
+        
+        let msg = "You scored \(points)"
         
         let alert = UIAlertController(title: "Hello world",
                                       message: msg,
@@ -32,6 +65,8 @@ class ViewController: UIViewController {
         
         alert.addAction(action)
         present(alert, animated: true, completion: nil)
+        
+        startNewRound()
     }
     
     @IBAction func sliderMoved(_ slider: UISlider) {
