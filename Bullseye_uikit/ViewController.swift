c9179c3ae34ba674c57a1b322617a2e5e58d5b01
aybjax <aybat93@gmail.com>
2021-05-29 23:22:41 +0600

    initial ui with listeners. bug present: initial value of slider cannot be set

--- Bullseye_uikit/ViewController.swift
+++ Bullseye_uikit/ViewController.swift
@@ -8,12 +8,35 @@
 import UIKit
 
 class ViewController: UIViewController {
+    // Properties
+    // ==========
+    
+    var currentValue: Int = 1
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
+    
+    // Methods
+    // =======
 
-
+    @IBAction func showAlert() {
+        let msg = "The value of the slider is: \(currentValue)"
+        
+        let alert = UIAlertController(title: "Hello world",
+                                      message: msg,
+                                      preferredStyle: .alert)
+        let action = UIAlertAction(title: "OK", style: .default, handler: nil)
+        
+        alert.addAction(action)
+        present(alert, animated: true, completion: nil)
+    }
+    
+    @IBAction func sliderMoved(_ slider: UISlider) {
+        currentValue = lroundf(slider.value)
+        print(currentValue)
+    }
 }
 
